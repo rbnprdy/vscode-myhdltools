794e2661a0aee6ea5197da051331662357e5bb92
Ruben Purdy <ruben.purdy@gmail.com>
2019-06-06 12:51:09 -0700

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,27 @@
+'use strict';
+import {DocumentSelector, ExtensionContext, commands} from "vscode";
+
+// ctags
+import {CtagsManager} from "./ctags";
+
+// Commands
+import * as TestbenchInstantiation from "./commands/TestbenchInstantiation"
+
+export let ctagsManager:CtagsManager = new CtagsManager;
+
+export function activate(context: ExtensionContext) {
+	console.log('"myhdltools" is now active!');
+
+	// document selector
+	let systemverilogSelector:DocumentSelector = { scheme: 'file', language: 'systemverilog' };
+	let verilogSelector:DocumentSelector = {scheme: 'file', language: 'verilog'};
+
+	// Configure ctags
+	ctagsManager.configure();
+
+	// Configure command to instantiate a module
+	commands.registerCommand("myhdltools.instantiateTestbench", TestbenchInstantiation.instantiateModuleInteract);
+}
+
+export function deactivate() {
+}
